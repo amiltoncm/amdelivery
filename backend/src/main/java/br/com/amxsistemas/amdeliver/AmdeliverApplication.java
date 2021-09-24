7dfe2d54207701b330643b20c44bec89f2914ff2
Amilton Machado <amilton.machado@fw7.com.br>
2021-09-23 21:05:43 -0300

    Project created

---
+++ backend/src/main/java/br/com/amxsistemas/amdeliver/AmdeliverApplication.java
@@ -0,0 +1,13 @@
+package br.com.amxsistemas.amdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/amxsistemas/amdeliver/AmdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amxsistemas.amdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
